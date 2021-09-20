d28e8c141980e3ca2db8c6e00796f1bd0de80bae
Guilherme Maciel <67983309+GuilhermeMacielJs@users.noreply.github.com>
2021-09-20 12:23:12 -0300

    Fundamentos do nodeJS. Introdução aos métodos get, post, putt e pacth. Introdução a API.

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+app.put('/courses/:id', (req,res)=>{
+    const body = req.params
+    console.log(body)
+    return res.json([
+        "curso 1",
+        "curso 2",
+        "curso 3",
+        "curso 4",
+        "curso 5",
+    ])
+})
+
+app.get('/courses', (req,res)=>{
+    const query = req.query
+    console.log(query)
+    return res.json([
+        "curso 1",
+        "curso 2",
+        "curso 3",
+        "curso 4",
+        "curso 5",
+    ])
+})
+
+app.listen(8080)
